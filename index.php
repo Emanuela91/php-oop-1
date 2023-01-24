00519995b19242b3fe7f019bfd599bbdf8abf4e0
Emanuela91 <emanuela.marson91@gmail.com>
2023-01-24 16:54:53 +0100

    creato array di movies

--- index.php
+++ index.php
@@ -4,14 +4,16 @@ class Movie
     public $title;
     public $length;
     public $lenguage;
+    public array $movies;
 
-    public function __construct($title, $length, $lenguage)
+    public function __construct($title, $length, $lenguage, array $movies)
     {
         $this->title = $title;
         $this->length = $length;
         $this->lenguage = $lenguage;
+        $this->movies = $movies;
     }
+
 }
 
-$movie1 = new Movie("Avatar", "125", "Eng");
-var_dump($movie1);
+$movies = [new Movie("Avatar", "125", "Eng"), new Movie("The Lord Of the Rings", "230", "Eng"), new Movie("Tre uomini e una gamba", "90", "Ita")];

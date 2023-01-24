367a1aeeb3f2a1915dca39cfeaff21e5331d0e1d
Emanuela91 <emanuela.marson91@gmail.com>
2023-01-24 18:02:23 +0100

    tolto array da movie, spostato foreach, creata metodo per html

--- index.php
+++ index.php
@@ -15,14 +15,12 @@
         public $title;
         public $length;
         public $lenguage;
-        // public array $movies;
-    
+
         public function __construct($title, $length, $lenguage)
         {
             $this->title = $title;
             $this->length = $length;
             $this->lenguage = $lenguage;
-            // $this->movies = $movies;
         }
 
         public function getfullMovie()
@@ -32,21 +30,28 @@
                 $this->lenguage;
         }
 
-    // public function getEachMovie()
-// {
-//     $str = "";
-//     foreach ($this->movies as $movie) {
-//         $str .= $movie->getfullMovie();
-//     }
-// }
+        public function getHtml()
+        {
+            return "Movie: " . $this->title .
+                "<br> Length: " . $this->length .
+                "<br> Lenguage: " . $this->lenguage;
+        }
+
     }
 
-    $movie = new Movie("Avatar", "125", "Eng");
+    // $movie = new Movie("Avatar", "125", "Eng");
+    
+    // echo $movie->getfullMovie();
+    
+    $movies = [new Movie("Avatar", "125", "Eng"), new Movie("The Lord Of the Rings", "230", "Eng"), new Movie("Tre uomini e una gamba", "90", "Ita")];
 
-    echo $movie->getfullMovie();
+    foreach ($movies as $movie)
+        $str = ""; {
+        $str .= $movie->getfullMovie();
+    }
 
-    // $movies = [new Movie("Avatar", "125", "Eng"), new Movie("The Lord Of the Rings", "230", "Eng"), new Movie("Tre uomini e una gamba", "90", "Ita")];
-    ?>
+    echo $movie->getHtml()
+        ?>
 
 
 </body>

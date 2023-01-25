53ce628cdb427345a1a6734f62e34c087819b5d2
Emanuela91 <emanuela.marson91@gmail.com>
2023-01-25 10:37:10 +0100

    aggiunti commenti e classe genere

--- index.php
+++ index.php
@@ -9,13 +9,39 @@
 </head>
 
 <body>
+    <h1>MOVIES</h1>
     <?php
+
+    // classe genere da aggiungere a movie 
+    class Genre
+    {
+        public $name;
+        public $description;
+
+        public function __construct($name, $description)
+        {
+            $this->name = $name;
+            $this->description = $description;
+        }
+
+        public function getHtml()
+        {
+            $name = $this->name;
+            $description = $this->description;
+
+            return $name . $description;
+        }
+    }
+
+    // classe movie in cui si crea la struttura dei singoli film 
     class Movie
     {
+        // dichiaro le variabili
         public $title;
         public $length;
         public $lenguage;
 
+        // costruttore della classe 
         public function __construct($title, $length, $lenguage)
         {
             $this->title = $title;
@@ -23,15 +49,17 @@
             $this->lenguage = $lenguage;
         }
 
+        // metodo per stampare a schermo le caratteristiche dei film 
         public function getHtml()
         {
-            return "Movie: " . $this->title .
-                "<br> Length: " . $this->length .
+            return "<h3>" . "Movie: " . $this->title . "</h3>" .
+                "Length: " . $this->length . "min" .
                 "<br> Lenguage: " . $this->lenguage . "<br>";
         }
 
     }
 
+    // VERSIONE A VARIABILI
     // $movie1 = new Movie("Avatar", "125", "Eng");
     // $movie2 = new Movie("The Lord Of the Rings", "230", "Eng");
     // $movie3 = new Movie("Tre uomini e una gamba", "90", "Ita");
@@ -42,7 +70,7 @@
     // echo "<br><br>";
     // echo $movie3->getHtml();
     
-
+    // VERSIONE AD ARRAY 
     $movies = [new Movie("Avatar", "125", "Eng"), new Movie("The Lord Of the Rings", "230", "Eng"), new Movie("Tre uomini e una gamba", "90", "Ita")];
 
     foreach ($movies as $movie) {
